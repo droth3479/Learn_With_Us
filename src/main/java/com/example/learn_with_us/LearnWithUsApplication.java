9241e910d05b3e435f058bcf912b8ea77c42aaf6
droth3479 <92330553+droth3479@users.noreply.github.com>
2023-04-04 16:09:20 +0300

    Initial commit

---
+++ src/main/java/com/example/learn_with_us/LearnWithUsApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_with_us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWithUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnWithUsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learn_with_us/LearnWithUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_with_us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWithUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
