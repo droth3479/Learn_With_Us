d461b17298f91c055f52e02ac6b3c6e4474fa14d
droth3479 <92330553+droth3479@users.noreply.github.com>
2023-05-14 20:56:49 -0400

    addded init function to add data to db (for testing purposes)

--- src/main/java/com/example/learn_with_us/LearnWithUsApplication.java
+++ src/main/java/com/example/learn_with_us/LearnWithUsApplication.java
@@ -1,15 +1,40 @@
 package com.example.learn_with_us;
 
+import com.example.learn_with_us.data.entity.*;
+import com.example.learn_with_us.data.entity.Class;
+import com.example.learn_with_us.data.repository.CourseRepository;
+import com.example.learn_with_us.data.service.ContentService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
 public class LearnWithUsApplication {
+    @Autowired
+    ContentService service = new ContentService();
 
     public static void main(String[] args) {
         SpringApplication.run(LearnWithUsApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        Course scienceCourse = new Course();
+        scienceCourse.setName("Physics");
+        scienceCourse.setFounder("Joe Shmoe");
+        scienceCourse.setSubject(Subject.SCIENCE);
+
+        BaseContent scienceVideo = new VideoContent();
+        scienceVideo.setSource("learnwithus/content/video/cokeandmentos");
+
+        Class scienceClass = new Class();
+        scienceClass.setName("Chemical Reactions");
+        scienceClass.setCourse(scienceCourse);
+        scienceClass.setContent(scienceVideo);
+
+        service.addCourse(scienceCourse);
+        service.addClassContent(scienceVideo);
+        service.addClass(scienceClass);
+    }
 }
