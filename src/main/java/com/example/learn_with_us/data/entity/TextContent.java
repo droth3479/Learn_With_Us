367344f88f1c59d6d5ff772dcbed44683bcba868
droth3479 <92330553+droth3479@users.noreply.github.com>
2023-05-22 20:58:45 -0400

    created component to return on call to display().

--- src/main/java/com/example/learn_with_us/data/entity/TextContent.java
+++ src/main/java/com/example/learn_with_us/data/entity/TextContent.java
@@ -1,8 +1,14 @@
 package com.example.learn_with_us.data.entity;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.textfield.TextArea;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * The TextContent class represents text-based content for a class.
@@ -18,6 +24,23 @@ public class TextContent extends BaseContent{
      */
     @Override
     public Component display() {
-        return null;
+        TextArea readonlyArea = new TextArea();
+        readonlyArea.setReadOnly(true);
+        readonlyArea.setValue(getText());
+
+        return readonlyArea;
+    }
+
+    private String getText() {
+        String fileAsString = "";
+
+        try {
+            final File file = new File(this.getSource());
+            fileAsString = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileAsString;
     }
 }
