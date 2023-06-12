7aa0182f18ad1e39c54ac9afc1454de16a6b6749
droth3479 <92330553+droth3479@users.noreply.github.com>
2023-06-12 16:23:25 -0400

    user bean to hold user object during session.

---
+++ src/main/java/com/example/learn_with_us/beans/UserBean.java
@@ -0,0 +1,24 @@
+package com.example.learn_with_us.beans;
+
+import com.example.learn_with_us.data.entity.User;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.VaadinSessionScope;
+
+/**
+ * The user bean represents the user currently logged in.
+ * It is set on login, and referenced by each page on navigation.
+ * The scope is the vaadin session, ie a browser window.
+ */
+@SpringComponent
+@VaadinSessionScope
+public class UserBean {
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
